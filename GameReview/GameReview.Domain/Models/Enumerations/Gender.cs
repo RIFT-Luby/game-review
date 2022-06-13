2477b83584614983a809a625826f77c7cad8cfd6
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-06-13 16:31:37 -0300

    - feat: Adicionando Game.cs e Gender.cs

---
+++ GameReview/GameReview.Domain/Models/Enumerations/Gender.cs
@@ -0,0 +1,19 @@
+﻿
+using GameReview.Domain.Core;
+
+namespace GameReview.Domain.Models.Enumerations
+{
+    public class Gender : Enumeration
+    {
+        public static Gender Action = new(1, nameof(Action));
+        public static Gender Race = new(2, nameof(Race));
+        public static Gender FPS = new(3, nameof(FPS));
+        public static Gender RPG = new(4, nameof(RPG));
+        public static Gender Strategy = new(5, nameof(Strategy));
+
+        public Gender(int id, string name)
+            :base (id, name)
+        {
+        }
+    }
+}
---
+++ GameReview/GameReview.Domain/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿
+using GameReview.Domain.Core;
+using GameReview.Domain.Models.Enumerations;
+
+namespace GameReview.Domain.Models
+{
+    public class Game : Register
+    {
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Developer { get; set; }
+        public Gender Gender { get; set; }
+        public int IdGenderType { get; set; }
+        public decimal Score { get; set; }
+        public string Img { get; set; }
+        public string Console { get; set; }
+    }
+}
