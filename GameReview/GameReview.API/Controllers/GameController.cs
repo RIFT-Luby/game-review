2e2474ed55d50745e0c81492154a3ae8a2db0e28
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-06-14 16:45:27 -0300

    - feat: Adicionando mappers e controller de game

---
+++ GameReview/GameReview.API/Controllers/GameController.cs
@@ -0,0 +1,55 @@
+﻿using GameReview.Application.Interfaces;
+using GameReview.Application.ViewModels.Game;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameReview.API.Controllers
+{
+    [ApiController]
+    [Route("api/games")]
+    public class GameController : ControllerBase
+    {
+
+        private readonly IGameService _gameService;
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        /* Em testes
+        [HttpGet]
+        public async IEnumerable<GameResquest> GetAllGames()
+        {
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<GameResquest> GetGame(int id)
+        {
+
+        }
+        */
+
+        [HttpPost]
+        public async Task<ActionResult<GameResquest>> Post([FromBody] GameResquest gameResquest)
+        {
+            return await _gameService.RegisterAsync(gameResquest);
+            
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<GameResquest>> Put([FromBody] GameResquest gameResquest, int id)
+        {
+            return await _gameService.UpdateAsync(gameResquest, id);
+        }
+
+        /* Em testes
+        [HttpDelete]
+        public async Task<ActionResult<GameResquest>> Delete(int id)
+        {
+            //return await _gameService.DeleteAsync();
+        }
+        */
+
+
+    }
+}
---
+++ GameReview/GameReview.Application/Mappers/DomainToRequest.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using GameReview.Application.ViewModels.Game;
+using GameReview.Domain.Models;
+
+namespace GameReview.Application.Mappers
+{
+    public class DomainToRequest : Profile
+    {
+        public DomainToRequest()
+        {
+            CreateMap<Game, GameResquest>().ReverseMap();
+        }   
+    }
+}
