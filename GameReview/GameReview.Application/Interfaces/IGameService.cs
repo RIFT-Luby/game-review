3d1df877d2ed3b86b644720e03c8252b4df55c48
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-06-14 11:36:21 -0300

    - feat: Adicionando interface e service para game (incompleto)

---
+++ GameReview/GameReview.Application/Interfaces/IGameService.cs
@@ -0,0 +1,19 @@
+﻿using GameReview.Application.ViewModels.Game;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace GameReview.Application.Interfaces
+{
+    public interface IGameService
+    {
+        Task<GameResquest> RegisterAsync(GameResquest gameResquest);
+        Task<GameResquest> UpdateAsync(GameResquest gameResquest, int id);
+        Task<GameResquest> DeleteAsync(GameResquest gameResquest);
+        Task<GameResquest?> FirstAsync(Expression<Func<GameResquest, bool>> filter, 
+            Func<IQueryable<GameResquest>, IIncludableQueryable<GameResquest, object>>? include = null);
+        Task<IEnumerable<GameResquest>> GetDataAsync(
+            Expression<Func<GameResquest, bool>>? filter = null, Func<IQueryable<GameResquest>,
+            IIncludableQueryable<GameResquest, object>>? include = null,
+            int? skip = null, int? take = null);
+    }
+}
---
+++ GameReview/GameReview.Application/Services/GameService.cs
@@ -0,0 +1,37 @@
+﻿
+using GameReview.Application.Interfaces;
+using GameReview.Application.ViewModels.Game;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace GameReview.Application.Services
+{
+    public class GameService : IGameService
+    {
+        public Task<GameResquest> RegisterAsync(GameResquest gameResquest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GameResquest> UpdateAsync(GameResquest gameResquest, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GameResquest> DeleteAsync(GameResquest gameResquest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GameResquest?> FirstAsync(Expression<Func<GameResquest, bool>> filter, Func<IQueryable<GameResquest>, IIncludableQueryable<GameResquest, object>>? include = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GameResquest>> GetDataAsync(Expression<Func<GameResquest, bool>>? filter = null, Func<IQueryable<GameResquest>, IIncludableQueryable<GameResquest, object>>? include = null, int? skip = null, int? take = null)
+        {
+            throw new NotImplementedException();
+        }
+       
+    }
+}
