4c75b3e7342bac9962d5233055fb987a2164331f
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-06-14 16:46:45 -0300

    - refactor: Adicionando DI para gameservice e fluent api

--- GameReview/GameReview.API/Configuration/DependencyInjectionConfig.cs
+++ GameReview/GameReview.API/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,19 @@
-﻿namespace GameReview.API.Configuration
+﻿using FluentValidation.AspNetCore;
+using GameReview.Application.Interfaces;
+using GameReview.Application.Services;
+using GameReview.Application.Validations;
+
+namespace GameReview.API.Configuration
 {
     public static class DependencyInjectionConfig
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection service)
         {
+            service.AddScoped<IGameService, GameService>();
+
+            service.AddFluentValidation(fv 
+                => fv.RegisterValidatorsFromAssemblyContaining<GameValidator>());
+
             return service;
         }
     }
