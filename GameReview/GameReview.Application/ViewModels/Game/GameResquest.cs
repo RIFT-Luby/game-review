31cb4692f55339d6709772691604b9a7461dfbc3
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-06-14 11:38:15 -0300

    - feat: Adicionando interface de gamerepository; Adicionando viewmodel de game

---
+++ GameReview/GameReview.Application/ViewModels/Game/GameResquest.cs
@@ -0,0 +1,16 @@
+﻿
+using GameReview.Application.ViewModels.GameGender;
+
+namespace GameReview.Application.ViewModels.Game
+{
+    public class GameResquest
+    {
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Developer { get; set; }
+        public GameGenderResponse GameGenderResponse { get; set; }
+        public int IdGenderType { get; set; }
+        public decimal Score { get; set; }
+        public string Console { get; set; }
+    }
+}
