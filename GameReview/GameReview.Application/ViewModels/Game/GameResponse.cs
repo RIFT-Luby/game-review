790784d2dea67666db243be906b579470ea8aa79
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-06-15 11:10:34 -0300

    - feat: Adicionando game response e modificação de gender

---
+++ GameReview/GameReview.Application/ViewModels/Game/GameResponse.cs
@@ -0,0 +1,17 @@
+﻿
+using GameReview.Application.ViewModels.GameGender;
+using GameReview.Domain.Core;
+
+namespace GameReview.Application.ViewModels.Game
+{
+    public class GameResponse : Register
+    {
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Developer { get; set; }
+        public GameGenderResponse GameGenderResponse { get; set; }
+        public int IdGenderType { get; set; }
+        public int Score { get; private set; }
+        public string Console { get; set; }
+    }
+}
