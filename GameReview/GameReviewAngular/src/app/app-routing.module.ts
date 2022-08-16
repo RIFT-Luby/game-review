0038415d28cad620c2ae3db9e28cbc90d09d4a5b
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2022-08-16 15:32:58 -0300

    - refactor: Atualizações em roteamento, services e módulos

--- GameReview/GameReviewAngular/src/app/app-routing.module.ts
+++ GameReview/GameReviewAngular/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
+import { ReviewComponent } from './pages/review/review.component';
+
+import { AuthGuard } from './../../src - Copia/app/shared/guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
 import { ApiHttpInterceptor } from './shared/interceptors/api-http.intercerptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard], children: [
+    {path: 'reviews', component: ReviewComponent, canActivate: [AuthGuard]}
+    ],
+  },
+  {path: '**', component: LoginComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GameReview/GameReviewAngular/src/app/app.module.ts
+++ GameReview/GameReviewAngular/src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { NavbarModule } from './shared/components/navbar/navbar.module';
+
+import { LoginModule } from './pages/login/login.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HomeModule } from './shared/components/home/home.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    HomeModule,
+    NavbarModule,
+    LoginModule,
+    MatSnackBarModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
